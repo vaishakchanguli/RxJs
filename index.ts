df67bd35b06a3bdaa738757d421069688efba40e
vaishakchanguli <vaishakch8@gmail.com>
2021-10-20 14:24:30 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -7,3 +7,9 @@ let observable = new Observable((subsciber) => {
   subsciber.next('chan');
 });
 console.log(observable);
+const subscription$ = observable.subscribe((value) => console.log(value));
+const subscription2$ = observable.subscribe((value) => console.log(value));
+console.log(subscription$);
+console.log('subscription2',subscription2$);
+subscription$.unsubscribe();
+console.log(subscription$);
