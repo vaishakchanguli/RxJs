3d25de7864e9162f28a6a31cc8532c36e253d4b8
vaishakchanguli <vaishakch8@gmail.com>
2021-10-21 11:08:17 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -3,13 +3,17 @@ import './style.css';
 import { of, map, Observable } from 'rxjs';
 
 let observable = new Observable((subsciber) => {
-  subsciber.next('vaishak');
-  subsciber.next('chan');
+  let counter = 1;
+  console.log('start');
+
+  setInterval(() => {
+    console.log('emit', counter);
+    subsciber.next(counter++);
+  }, 10000);
+
+  subsciber.complete();
 });
-console.log(observable);
-const subscription$ = observable.subscribe((value) => console.log(value));
-const subscription2$ = observable.subscribe((value) => console.log(value));
-console.log(subscription$);
-console.log('subscription2',subscription2$);
-subscription$.unsubscribe();
-console.log(subscription$);
+
+let subscription = observable.subscribe({
+  next: (value) => console.log(value),
+});
