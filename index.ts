02f6cce7e3af0295b94c981cd8fa546dda1a1385
vaishakchanguli <vaishakch8@gmail.com>
2021-10-28 18:22:48 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-const triggerButton = document.querySelector('button#trigger');
+import { interval } from 'rxjs';
+import { take } from 'rxjs/operators';
+
+interval(2000)
+  .pipe(take(5))
+  .subscribe({
+    next: (value) => console.log(value),
+    complete: () => console.log('Complete'),
+  });
+
+// Logs:
+// 0
+// Completed
