7b6a5f2b5a6ac8eb6104ae55342d6b9733549df8
vaishakchanguli <vaishakch8@gmail.com>
2021-11-08 11:53:14 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -1,10 +1,31 @@
-import { zip, of, from } from 'rxjs';
+import { zip, of, from, Observable, combineLatest } from 'rxjs';
+import { take } from 'rxjs';
 
-let age$ = of(27, 25);
-let name$ = from(['Foo', 'Bar', 'Beer']);
-let isDev$ = of(true, true, false, false);
-
-zip([age$, name$, isDev$]).subscribe({
-  next: (value) => console.log(value),
-  complete: () => console.log('Complete'),
+let age$ = new Observable((subscriber) => {
+  subscriber.next(1);
+  subscriber.next(2);
+  subscriber.next(3);
+  return () => {
+    console.log('age$ teardown');
+  };
 });
+let name$ = new Observable((subscriber) => {
+  subscriber.next('Foo');
+  subscriber.complete();
+  subscriber.next('Bar');
+  subscriber.next('beer');
+  return () => {
+    console.log('name$ teardown');
+  };
+});
+
+let combine$ = combineLatest([age$, name$])
+  .pipe(take(2))
+  .subscribe({
+    next: (value) => console.log(value),
+    complete: () => console.log('Complete'),
+  });
+
+setTimeout(() => {
+  combine$.unsubscribe();
+}, 2000);
