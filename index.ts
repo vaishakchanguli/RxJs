abedfd22347010c132623f381adc9d39411b2717
vaishakchanguli <vaishakch8@gmail.com>
2021-11-17 12:23:56 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -1,9 +1,15 @@
-import { Observable } from 'rxjs';
-import { map, tap } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
+import { concatMap } from 'rxjs/operators';
 
-import { interval } from 'rxjs';
-import { debounce } from 'rxjs/operators';
+const source$ = new Observable((subsciber) => {
+  setTimeout(() => subsciber.next('A'), 2000);
+  setTimeout(() => subsciber.next('B'), 5000);
+});
 
-interval(1000)
-  .pipe(debounce((i) => interval(200 * i)))
-  .subscribe((value) => console.log('Next', value));
+console.log('Subscribed');
+source$
+.pipe(concatMap((value) => of(1, 2))) //will concat outer to inner obs.
+.subscribe({
+  next: (value) => console.log(value),
+  complete: () => console.log('Completed'),
+});
