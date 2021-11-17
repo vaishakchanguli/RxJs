1a667dcb6f3e2e2fa6cfd26e19c534c2d1f4c73a
vaishakchanguli <vaishakch8@gmail.com>
2021-11-17 20:10:36 +0530

    2 files modified

--- index.ts
+++ index.ts
@@ -1,15 +1,3 @@
-import { Observable, of, tap } from 'rxjs';
-import { concatMap } from 'rxjs/operators';
-
-const source$ = new Observable((subsciber) => {
-  setTimeout(() => subsciber.next('A'), 2000);
-  setTimeout(() => subsciber.next('B'), 5000);
-});
-
-console.log('Subscribed');
-source$
-.pipe(concatMap((value) => of(1, 2))) //will concat outer to inner obs.
-.subscribe({
-  next: (value) => console.log(value),
-  complete: () => console.log('Completed'),
-});
+const endpointInput: HTMLInputElement =
+  document.querySelector('input#endpoint');
+const fetchButton = document.querySelector('button#fetch');
