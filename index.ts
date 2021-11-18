5b740da4b36fd08f0131dfee2731ae030a662d37
vaishakchanguli <vaishakch8@gmail.com>
2021-11-18 09:58:16 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -1,3 +1,15 @@
-const endpointInput: HTMLInputElement =
-  document.querySelector('input#endpoint');
-const fetchButton = document.querySelector('button#fetch');
+import { interval, of, throwError } from 'rxjs';
+import { concatMap } from 'rxjs/operators';
+
+interval(1000)
+  .pipe(
+    concatMap((value) => {
+      if (value % 3) return of(value);
+      else return throwError(() => 'Error');
+    })
+  )
+  .subscribe({
+    next: (value) => console.log(value),
+    error: (error) => console.log(error),
+    complete: () => console.log('Completed'),
+  });
