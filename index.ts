80375247056aa355bb1bcad6fb9aadce5c474aa4
vaishakchanguli <vaishakch8@gmail.com>
2021-11-18 10:05:01 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
-import { interval, of, throwError } from 'rxjs';
-import { concatMap } from 'rxjs/operators';
+import { EMPTY, interval, of, throwError } from 'rxjs';
+import { catchError, concatMap, take } from 'rxjs/operators';
 
 interval(1000)
   .pipe(
     concatMap((value) => {
       if (value % 3) return of(value);
-      else return throwError(() => 'Error');
-    })
+      else return throwError(() => 'Error').pipe(catchError(() => EMPTY));
+    }),
+    take(5)
   )
   .subscribe({
     next: (value) => console.log(value),
