4d6aff56325d1716175ead626bde26c5fb79469c
vaishakchanguli <vaishakch8@gmail.com>
2021-12-01 18:22:37 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -1,15 +1,23 @@
-import { from, of, concat, interval, merge } from 'rxjs';
+import { from, of, concat, interval, merge, timer } from 'rxjs';
 import { take, tap, delay, concatMap } from 'rxjs/operators';
 
 let interval$ = interval(1000).pipe(take(5));
-let timer$ = of('hello').pipe(
-  tap((value) => console.log('tap', value)),
-  take(1)
-);
+let timer$ = of('hello').pipe(tap((value) => console.log('tap', value)));
+
+// merge(timer$, interval$).subscribe({
+//   next: (value) => {
+//     console.log(value);
+//   },
+//   complete: () => console.log('completed'),
+// });
 
 merge(timer$, interval$).subscribe({
   next: (value) => {
     console.log(value);
+    timer(1).subscribe(() => {});
   },
-  complete: () => console.log('completed'),
+  error: () => {},
+  complete: () => {
+    console.log('complete');
+  },
 });
